780d9babc76c178240c69b41abcb793ead39bcc5
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-12 22:35:08 -0400

    Add files via upload

---
+++ webpack.dev.js
@@ -0,0 +1,55 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+module.exports = {
+    entry:'./src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'var',
+        library: 'Client',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        }),
+        new CleanWebpackPlugin({
+            //Simulate the removal of file:
+        dry: true,
+        //write logs to console
+        verbose:false,
+        //automatically remove all unused webpack assets on rebuild
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets:false
+        })
+    ]
+
+
+};
+
+
+
+
