06d595b080fade8dc23cdadf22c431b1b7425dbf
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-13 17:37:19 -0400

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const WbApiKey = process.env.WB_APIKEY;
+const PbApiKey = process.env.PB_APIKEY;
+const gnUser = process.env.GN_USERNAME;
+
+const allReq = require('./allAPIrequests');
+
+// Require Express to run server and routes
+const express = require('express');
+
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+const bodyParser = require('body-parser');
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('dist'));
+
+console.log(__dirname);
+
+// Setup Server
+port = 8000;
+app.listen(8000, function (){
+    console.log('Server running on port 8000!');
+});
+
+
+
+//TODO routes
+app.get('/', function (req, res){
+    res.send('src/server/server.js');
+});
+
+
+app.post('/weather', allReq.weatherbitInfo);
+app.post('/image', allReq.pixabayInfo);
+app.post('/location', allReq.geoNamesInfo);
+
+
+module.exports = server;
+
+
