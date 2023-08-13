1106e142268a43a81e4b089a9232b3bfdb18cf50
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-13 17:35:42 -0400

    Add files via upload

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,87 +1,85 @@
-/* Global Variables */
 
-/* Base URLs */ 
-const geoNames = 'http://api.geonames.org/search?';
-const weatherbit = 'https://api.weatherbit.io/v2.0/history/daily';
-const pixabay = "https://pixabay.com/api/";
-
-
-
-const city = document.getElementById('city').value;
-const country = document.getElementById('country').value;
-const startDate = document.getElementById('tripDate').value;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+1+ '.' + d.getDate()+'.'+ d.getFullYear();
 
+let inputCity = document.getElementById('city').value;
+let inputCountry = document.getElementById('country').value;
+const inputDate = document.getElementById('tripDate').value;
 
-    
 /* Function called by event listener */
 async function performCallBack(event){
     event.preventDefault();
-
-    date= newDate;
-    //date countdown function(?)
-
-    const response =  await geoNamesInfo(city, country);
-    const response2 = await weatherbitInfo();
-    const response3 = await pixabayInfo();
+   
+    console.log (`${inputCity}, ${inputCountry} leaving on ${inputDate}` );
     
-    // postData('/addNew',  {temp:data.main.temp, feelings:feelings, date:date});
-    // retrieveData();
-  
+    //updateUI();
 }
 
-//call GeoNames
-const geoNamesInfo = async (city, country) => {
-    //call API
-    const response = await fetch(`${geoNames}q=${city}&country=${country}&username=mitchelle82`);
-    try {
-        const data =await response.json();
-        console.log(data);
-        return data;
-    } catch(error){
-        console.log("error", error);
-    }
-   
-};
-//call Weatherbit
-const weatherbitInfo = async (city, country, WbApiKey) => {
-    const response = await fetch(`${weatherbit}&city=${city}&country=${country}&start_date=${startDate}&key=${WbApiKey}`);
-    try {
-        const data =await response.json();
-        console.log(data);
-        return data;
-    } catch(error){
-        console.log("error", error);
-    }
-};
 
-//call Pixabay 
-const pixabayInfo = async (city, country, PbApiKey) => {
-    const response = await fetch(`${pixabay}?key=${PbApiKey}&q=${city}+${country}&image_type=photo&category=travel`);
-    try {
-        const data =await response.json();
-        console.log(data);
-        return data;
-    } catch(error){
-        console.log("error", error);
-    }
+
+
+
+
+
+const countdown = () => {
+    const currentDate = Date.now();
+    const timeLeft =currentDate - inputDate;
+    return timeLeft;
+    
+    
 };
 
 
 
-/* Function to POST data */
-const postData = async ( url = '', data = {})=>{
+const weatherData = async (latitude, longitude)=>{
 
-    const response = await fetch('http://localhost:8000/', {
+    const response = await fetch('http://localhost:8000/weather', {
     method: 'POST', 
-    credentials: 'same-origin', 
+  
     headers: {
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    body: JSON.stringify({latitude: latitude, longitude: longitude}), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+const imageData = async (city, country)=>{
+
+    const response = await fetch('http://localhost:8000/image', {
+    method: 'POST', 
+  
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({city:city, country:country}), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      return newData.hits[0].largeImageURL;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+const locationData = async (city, country)=>{
+
+    const response = await fetch('http://localhost:8000/location', {
+    method: 'POST', 
+  
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({city:city, country:country}), // body data type must match "Content-Type" header        
   });
 
     try {
@@ -93,21 +91,21 @@ const postData = async ( url = '', data = {})=>{
 };
 
 
-
-
-/* Function to update UI*/ 
-const retrieveData = async () => {
-    const request = await fetch('/');
-    try {
-        const allData =  await request.json();
-        console.log(allData);
-        document.getElementById('imgLocation').innerHTML = allData.imgLocation;
-        document.getElementById('city').innerHTML = 'My trip to' + allData.city;
-        document.getElementById('weather').innerHTML = 'The weather will be' + allData.weather;
-        document.getElementById('countdown').innerHTML = 'There are' + allData.countdown + 'day left';
-    }
-    catch(error) {
-        console.log('error');
-    }
+/* Function to update UI*/
+const updateUI = () => {
+    document.getElementById('imgLocation').appendChild('img');
+    document.getElementById('city').innerHTML = `My trip to ${city}, ${country}`;
+    document.getElementById('weather').innerHTML = `The weather will be' + ${weather}`;
+    document.getElementById('countdown').innerHTML = `There are ${countdown} days left till your trip!`;
 };
+       
 
+const clearButton = () => {
+    city.remove(); //This is deleting input field, not the text, which is not what I want it to do
+
+}   
+
+
+export { performCallBack};
+export { updateUI };
+export { clearButton };
