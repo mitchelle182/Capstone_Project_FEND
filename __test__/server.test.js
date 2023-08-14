1197c3a1bb8584bf63db8f15d03f61e82a271bf5
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-14 19:14:36 -0400

    Delete __test__ directory

--- __test__/server.test.js
+++
@@ -1,8 +0,0 @@
-import server from './src/server/server.js';
-import request from 'supertest';
-
-describe('server', function () {
-  it('loads', function (done) {
-    request(server).get('/').expect(200, done);
-  });
-});
