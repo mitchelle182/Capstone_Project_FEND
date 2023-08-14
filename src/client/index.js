fb1c6e1898d4ebbe4b1cd05b2b9a989a14fe6dce
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-14 19:38:07 -0400

    Add files via upload

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,9 @@
 import './styles/style.scss';
 
-import { performCallBack } from './js/app';
+import { performCallBack, updateUI, clearButton } from './js/app';
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performCallBack);
+
+
+
