b237c9e6ca166c65ffc907bf53148dcfd802b687
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-15 09:20:18 -0400

    Add files via upload

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,23 +1,20 @@
+
+
 /* Function called by event listener */
 async function performCallBack(event){
     event.preventDefault();
-   
-    
     let inputCity = document.getElementById('city').value;
     let inputDate = document.getElementById('date').value;
-    
-    
     console.log ("input submitted");
     
-      const response1 = await postLocation(inputCity);
-      const response2 = await postWeather(inputDate);
-      const response3 = await postImage(inputCity);
-      console.log(response1, response2, response3);
-      //updateUI();
+    postLocation(inputCity);
+    postWeather (inputCity, inputDate);
+    postImage(inputCity);
+    updateUI();
 }
 
 
-//send to geoNames data to backend 
+//send geoNames data to backend 
 const postLocation = async (city)=>{
   const data = {city};
   const response = await fetch('http://localhost:8000/', {
@@ -40,9 +37,9 @@ const postLocation = async (city)=>{
 
 
 
-//send to backend
+//send to weatherbit data to backend
 const postWeather = async (city, date)=>{
-    
+    const data = {city, date}
     const response = await fetch('http://localhost:8000/', {
     method: 'POST', 
   
@@ -61,26 +58,40 @@ const postWeather = async (city, date)=>{
 };
 
 
-//send to backend
+//send to pixabay data to backend
 const postImage = async (city, country)=>{
-
+    const data = {city, country}
     const response = await fetch('http://localhost:8000/', {
     method: 'POST', 
   
     headers: {
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify({city:city, country:country}), // body data type must match "Content-Type" header        
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
   });
 
     try {
       const newData = await response.json();
-      return newData.hits[0].largeImageURL;
+      return newData;
     }catch(error) {
     console.log("error", error);
     }
 };
-
+// /* Function to update UI*/
+const updateUI = async () => {
+  const request = await fetch('/');
+  try {
+      const allData =  await request.json();
+      console.log(allData);
+      document.getElementById('location').innerHTML = allData.location;
+      document.getElementById('tripDate').innerHTML = allData.tripDate;
+      document.getElementById('weather').innerHTML = allData.weather;
+      document.getElementById('imgLocation').innerHTML = allData.imgLocation;
+  }
+  catch(error) {
+      console.log('error');
+  }
+};
 
 
 export { performCallBack};
@@ -91,14 +102,7 @@ export { performCallBack};
 //    // let d = new Date();
 //   // let newDate = d.getMonth()+1+ '.' + d.getDate()+'.'+ d.getFullYear();
 
-// /* Function to update UI*/
-// const updateUI = () => {
-   
-//       document.getElementById('city').innerHTML = `Your upcoming trip to ${city}, ${country}`;
-//       // document.getElementById('weather').innerHTML =`The ${weather} will be`;
-//       // document.getElementById('countdown').innerHTML = ;
 
-// };
        
 
  
