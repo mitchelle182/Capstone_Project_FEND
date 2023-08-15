56916e2d6c82cc409fd54fa253f8a39ac099b5b2
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-08-15 19:30:34 -0400

    Add files via upload

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,16 +5,31 @@ async function performCallBack(event){
     event.preventDefault();
     let inputCity = document.getElementById('city').value;
     let inputDate = document.getElementById('date').value;
+    let d = new Date();
+    let newDate = d.getMonth()+1+ '.' + d.getDate()+'.'+ d.getFullYear();
     console.log ("input submitted");
     
-    postLocation(inputCity);
-    postWeather (inputCity, inputDate);
-    postImage(inputCity);
-    updateUI();
+    const response = await postLocation(inputCity, inputDate);
+    console.log(response);
+    let image = document.getElementById('imgLocation');
+    let cityName = response.location.name;
+    let countryName = response.location.countryName;
+    let weather = response.weather.high_temp;
+    let weatherDesc = response.weather.weather.description;
+    const src = response.photo.largeImageURL; 
+    console.log(src);
+    console.log(image);
+    
+    document.getElementById('location').innerHTML = `Your trip to ` + `${cityName}, ${countryName}`;
+    document.getElementById('weather').innerHTML = `It is currrently ` + `${weather} degrees Celsius, with ${weatherDesc}.` ;
+    document.getElementById('tripDate').innerHTML = `You will be leaving on ` + inputDate;
+    
+    //document.getElementById('imgLocation').append();
+    //updateUI();
 }
 
 
-//send geoNames data to backend 
+//send geoNames data to backend to get back the data
 const postLocation = async (city)=>{
   const data = {city};
   const response = await fetch('http://localhost:8000/', {
@@ -77,21 +92,8 @@ const postImage = async (city, country)=>{
     console.log("error", error);
     }
 };
-// /* Function to update UI*/
-const updateUI = async () => {
-  const request = await fetch('/');
-  try {
-      const allData =  await request.json();
-      console.log(allData);
-      document.getElementById('location').innerHTML = allData.location;
-      document.getElementById('tripDate').innerHTML = allData.tripDate;
-      document.getElementById('weather').innerHTML = allData.weather;
-      document.getElementById('imgLocation').innerHTML = allData.imgLocation;
-  }
-  catch(error) {
-      console.log('error');
-  }
-};
+
+
 
 
 export { performCallBack};
@@ -99,8 +101,7 @@ export { performCallBack};
 
 // let currentDate = Date.now();  
 // let daysLeft = currentDate - inputDate;
-//    // let d = new Date();
-//   // let newDate = d.getMonth()+1+ '.' + d.getDate()+'.'+ d.getFullYear();
+//    
 
 
        
